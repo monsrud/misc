7e51f63737e4b80adb08eb30cbca31fa8d5dab98
Marshall Onsrud <marshall.onsrud@gmail.com>
2023-10-27 12:30:20 -0500

    updates

--- bash/clone.sh
+++ bash/clone.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/ash
+
+mount /dev/sda2 /mnt
+rm -Rf /mnt/var/lib/cloud/*
+cp 01-new-machine-id.cfg /mnt/etc/cloud/cloud.cfg.d/
+umount /mnt
+
+fsck.ext2 /dev/sda2
 dd if=/dev/sda of=sda.mbr count=1 bs=512
 sfdisk -d /dev/sda > sda-ext.sf
 partclone.fat32 --clone --source /dev/sda1 | lz4 > sda1.fat32.lz4
